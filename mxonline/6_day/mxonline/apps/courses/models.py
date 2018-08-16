6619c9249b9cdeac4d3a80e4435b2b9f0a4ec4c6
hong <1816635208@qq.com>
2018-08-16 17:54:56 +0800

    用户重置密码

---
+++ mxonline/6_day/mxonline/apps/courses/models.py
@@ -0,0 +1,82 @@
+from django.db import models
+from datetime import datetime
+# Create your models here.
+
+
+class Course(models.Model):
+    DEGREE_CHOICES = (
+        ("cj", "初级"),
+        ("zj", "中级"),
+        ("gj", "高级")
+    )
+    name = models.CharField(max_length=50, verbose_name="课程名")
+    desc = models.CharField(max_length=300, verbose_name="课程描述")
+    # TextField类型可写任意长度的字符，后面会用富文本类型替代
+    detail = models.TextField(verbose_name="课程详情")
+    degree = models.CharField(choices=DEGREE_CHOICES, max_length=5)
+    learn_times = models.IntegerField(default=0, verbose_name="学校时长(分钟)")
+    students = models.IntegerField(default=0, verbose_name="学习人数")
+    fav_nums = models.IntegerField(default=0, verbose_name="收藏人数")
+    # 课程的封面图片
+    image = models.ImageField(upload_to="courses/%Y/%m", verbose_name="封面图", max_length=100)
+    click_nums = models.IntegerField(default=0, verbose_name="点击数")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = "课程"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+
+
+# 定义章节类,一个课程对应多个章节
+class Lesson(models.Model):
+    # 定义一个字段来让我们知道这个章节对应那个课程
+    course = models.ForeignKey(Course, verbose_name="课程", on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, verbose_name="章节名")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = "章节"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return "{0}--{1}".format(self.course, self.name)
+
+
+# 定义每个章节的视频类,一个章节对应多个视频
+class Video(models.Model):
+    # 定义一个字段来存储，让我们知道这个视频对应哪个章节
+    lesson = models.ForeignKey(Lesson, verbose_name="章节", on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, verbose_name="视频名")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = "视频"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return "{0}--{1}".format(self.lesson, self.name)
+
+
+# 课程资源，一个课程有很多资源
+class CourseResource(models.Model):
+    # 定义一个字段来让我们知道这个资源对应哪个课程
+    course = models.ForeignKey(Course, verbose_name="课程", on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, verbose_name="名称")
+    # 资源下载地址，FileField类型在后台管理系统中会自带上传按钮，底层也是一个字符串类型，要指定最大长度
+    download = models.FileField(
+        upload_to="course/resource/%Y/%m",
+        verbose_name="资源文件",
+        max_length=100
+    )
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = "课程资源"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return "{0}--{1}".format(self.course, self.name)
+
