c76b8e0f0e23b35797e264f27cb151889cc7bfcd
hong <1816635208@qq.com>
2018-09-05 14:40:26 +0800

    第15天

---
+++ mxonline/15_day/mxonline/apps/operation/models.py
@@ -0,0 +1,94 @@
+# coding = utf-8
+from django.db import models
+from datetime import datetime
+from users.models import UserProfile
+from courses.models import Course
+
+
+# 用户咨询表单
+class UserAsk(models.Model):
+    name = models.CharField(max_length=20, verbose_name="姓名")
+    mobile = models.CharField(max_length=11, verbose_name="手机")
+    course_name = models.CharField(max_length=50, verbose_name="课程名")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = "用户咨询"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return '用户：{0} 手机号：{1}'.format(self.name, self.mobile)
+
+
+# 用户对课程的评论，这个其实是users和Courses的多对多关系,所以额外定义一个表
+class CourseComments(models.Model):
+    # 会涉及两个外键：用户和课程，需要import进来
+    course = models.ForeignKey(Course, verbose_name="课程", on_delete=models.CASCADE)
+    user = models.ForeignKey(UserProfile, verbose_name="用户", on_delete=models.CASCADE)
+    comments = models.CharField(max_length=250, verbose_name="评论")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="评论时间")
+
+    class Meta:
+        verbose_name = "课程评论"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return '用户：{0}对于{1}的评论'.format(self.user, self.course)
+
+
+# 用户收藏,页面上分为三类：课程，讲师，机构
+class UserFavorite(models.Model):
+
+    TYPE_CHOICES = (
+        (1, "课程"),
+        (2, "课程机构"),
+        (3, "讲师")
+    )
+    user = models.ForeignKey(UserProfile, verbose_name="用户", on_delete=models.CASCADE)
+    # 传统的方法需要涉及4个外键：用户，课程，机构，讲师
+    # course = models.ForeignKey(Course, verbose_name="课程")
+    # teacher = models.ForeignKey()
+    # org = models.ForeignKey()
+    # 其实我们可以定义一个用户收藏类型id来简化操作，这个收藏类型可以是课程，机构或者讲师
+    fav_id = models.IntegerField(default=0, verbose_name="收藏类型ID")
+    fav_type = models.IntegerField(choices=TYPE_CHOICES, default=1, verbose_name="收藏类型")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="收藏时间")
+
+    class Meta:
+        verbose_name = "用户收藏"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return '用户{0}收藏了{1}'.format(self.user, self.fav_type)
+
+
+# 用户消息表,就是发送给用户的消息
+class UserMessage(models.Model):
+    # 使用整形ID来表示用户，0表示发送给所有用户，不用0就是发给单独用户的ID号
+    user = models.IntegerField(default=0, verbose_name="消息接收用户")
+    message = models.CharField(max_length=500, verbose_name="消息内容")
+    # False表未读，True表已读
+    has_read = models.BooleanField(default=False, verbose_name="是否已读")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = "用户消息"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return '用户{0}接收了{1}'.format(self.user, self.message)
+
+
+# 用户课程表,涉及两个外键：用户，课程
+class UserCourse(models.Model):
+    course = models.ForeignKey(Course, verbose_name="课程", on_delete=models.CASCADE)
+    user = models.ForeignKey(UserProfile, verbose_name="用户", on_delete=models.CASCADE)
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = "用户课程"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return '用户{0}学习了{1}'.format(self.user, self.course)
+
---
+++ mxonline/15_day/mxonline/apps/organization/models.py
@@ -0,0 +1,82 @@
+from django.db import models
+from datetime import datetime
+
+
+# Create your models here.
+# 定义城市类，可通过城市来筛选机构
+class City(models.Model):
+    name = models.CharField(max_length=20, verbose_name="城市")
+    desc = models.CharField(max_length=200, verbose_name="城市描述")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = "城市"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+
+
+# 定义课程机构
+class CourseOrg(models.Model):
+    city = models.ForeignKey(City, verbose_name="所在城市", on_delete=models.CASCADE)
+    name = models.CharField(max_length=50, verbose_name="机构名称")
+    desc = models.TextField(verbose_name="机构描述")
+    # upload_to就是保存到哪个位置，没有的话会自动创建，后面的%Y,%m分别是创建年月的文件夹
+    image = models.ImageField(upload_to="org/%Y/%m", verbose_name="封面图", max_length=100)
+    address = models.CharField(max_length=150, verbose_name="机构地址")
+    click_nums = models.IntegerField(default=0, verbose_name="点击数")
+    fav_nums = models.IntegerField(default=0, verbose_name="收藏数")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+    ORG_CHOICES = (
+        ("pxjg", "培训机构"),
+        ("gx", "高校"),
+        ("gr", "个人"),
+    )
+    category = models.CharField(choices=ORG_CHOICES, verbose_name="机构类别", max_length=20, default="pxjg")
+    students = models.IntegerField(default=0, verbose_name="学习人数")
+    course_nums = models.IntegerField(default=0, verbose_name="课程数")
+
+    class Meta:
+        verbose_name = "课程机构"
+        verbose_name_plural = verbose_name
+
+    # 获取机构教师数
+    def get_teacher_nums(self):
+        return self.teacher_set.all().count()
+
+    def __str__(self):
+        return self.name
+
+
+# 讲师,一个机构有很多讲师
+class Teacher(models.Model):
+    # 定义一个字段来指明讲师是哪个机构的
+    org = models.ForeignKey(CourseOrg, verbose_name="所属机构", on_delete=models.CASCADE)
+    name = models.CharField(max_length=50, verbose_name="教师名称")
+    work_years = models.IntegerField(default=0, verbose_name="工作年限")
+    work_company = models.CharField(max_length=50, verbose_name="就职公司")
+    work_position = models.CharField(max_length=50, verbose_name="公司职位")
+    points = models.CharField(max_length=50, verbose_name="教学特点")
+    click_nums = models.IntegerField(default=0, verbose_name="点击数")
+    fav_nums = models.IntegerField(default=0, verbose_name="收藏数")
+    image = models.ImageField(upload_to="teacher/%Y/%m", verbose_name="头像", max_length=100, default='')
+    age = models.IntegerField(default=18, verbose_name="年龄")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = "教师"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+
+    # 在courses/models.py->Courses中，定义了一个外键指向Teacher
+    def get_course_nums(self):
+        return self.course_set.all()
+
+
+
+
+
+
